0fb68080c2e3dab0f4fd86f1bc7629e814f91785
root <root@rtun2.lab.andrgaar>
2022-11-01 20:18:26 +0100

    modified:   server.py

--- rtun-routing/server.py
+++ rtun-routing/server.py
@@ -1,11 +1,15 @@
 import socket
+import time
 import main
 import select
 import threading
 
 from torpy.cells import CellRelayEnd, CellDestroy, CellRelaySendMe, StreamReason
+from torpy.circuit import TorCircuit, TorCircuitState
+from torpy.guard import TorGuard
 
-from router import TorRouter
+import lsr
+from lsr import ReceiveThread, SendThread, HeartBeatThread
 
 def rcv_data(rcv_sock, rcv_cn, extend_node):
     b = rcv_sock.recv_cell()
@@ -27,17 +31,35 @@ def rcv_data(rcv_sock, rcv_cn, extend_node):
 
     return cellrelaydata.data
 
-def snd_data(rsp, circuit_id, extend_node, rcv_cn, rcv_sock):
+def snd_data(rsp, circuit_id, extend_node, rcv_cn, rcv_sock, stream_id=0):
     inner_cell = main.CellRelayData(rsp, circuit_id)
-    relay_cell = main.CellRelay(inner_cell, stream_id=3, circuit_id=circuit_id, padding=None)
+    relay_cell = main.CellRelay(inner_cell, stream_id=stream_id, circuit_id=circuit_id, padding=None)
 
     extend_node.encrypt_forward(relay_cell)
     rcv_cn.encrypt_forward(relay_cell)
     rcv_sock.send_cell(relay_cell)
+    print("Sent cell:" + str(inner_cell))
 
 
 def list_rend_server(cookie, router_nick):
 
+    # Start router
+    lsr.start_router("P2", 5000)
+
+    receiver_thread = ReceiveThread("RECEIVER", lsr.threadLock)
+    sender_thread = SendThread("SENDER", lsr.threadLock)
+
+    HB_message = [{'RID' : lsr.global_router['RID']}]
+    heartbeat_thread = HeartBeatThread("HEART BEAT", HB_message, lsr.threadLock)
+                
+    receiver_thread.start()
+    sender_thread.start()
+    heartbeat_thread.start()
+                
+    lsr.threads.append(receiver_thread)
+    lsr.threads.append(sender_thread)
+    lsr.threads.append(heartbeat_thread)
+    
     print("Calling connect_to_rendezvous_point")
     rcv_sock, rcv_cn, circuit_id = main.connect_to_rendezvous_point(router_nick, cookie)
 
@@ -64,54 +86,63 @@ def list_rend_server(cookie, router_nick):
     rcv_cn.decrypt_backward(b)
     extend_node.decrypt_backward(b)
     cellbegin = b.get_decrypted()
-    print(cellbegin)
+    stream_id = b.stream_id
+    print("CellRelay received, stream_id: " + str(stream_id))
+    print(cellbegin.address)
+    print(cellbegin.port)
 
-    #print("Starting a TorRouter thread")
-    #router = TorRouter("127.0.0.1", 5000)
+
 
     print("Creating socket")
     try:
         s = create_sock(cellbegin.address, cellbegin.port)
-    except Exception:
+    except Exception as e:
+        print("Error creating socket: " + str(e))
         inner_cell = main.CellRelayEnd(StreamReason(7), circuit_id)
-        relay_cell = main.CellRelay(inner_cell, stream_id=1, circuit_id=circuit_id, padding=None)
+        relay_cell = main.CellRelay(inner_cell, stream_id=stream_id, circuit_id=circuit_id, padding=None)
         extend_node.encrypt_forward(relay_cell)
         rcv_cn.encrypt_forward(relay_cell)
         rcv_sock.send_cell(relay_cell)
     
         raise Exception
 
-    print("Connection OK")
+    print("Stream opened successfully")
     inner_cell = main.CellRelayConnected("127.0.0.1", 5000, circuit_id)
-    relay_cell = main.CellRelay(inner_cell, stream_id=1, circuit_id=circuit_id, padding=None)
+    relay_cell = main.CellRelay(inner_cell, stream_id=stream_id, circuit_id=circuit_id, padding=None)
     extend_node.encrypt_forward(relay_cell)
     rcv_cn.encrypt_forward(relay_cell)
     rcv_sock.send_cell(relay_cell)
 
-    print("Sending CellNetInfo")
-    #rcv_sock.send_cell(CellNetInfo(int(time.time()), "10.1.0.1", "10.2.0.2"))
-    rcv_sock.send_cell(relay_cell)
-    time.sleep(10)
+    # Add neighbour
+    lsr.add_neighbour("P1", 100, '127.0.0.1', 5000, None, circuit_id, None, stream_id, 
+                        receive_node=rcv_cn, extend_node=extend_node, receive_socket=rcv_sock)
 
     print("Socket select")
-
     while True:
-        r, w, _ = select.select([rcv_sock.ssl_socket, s], [], [])
-        if rcv_sock.ssl_socket in r:
-            buf = rcv_data(rcv_sock, rcv_cn, extend_node)
-            if buf == 404:
-                continue
-            if len(buf) == 0:
-                break
-            s.send(buf)
-        if s in r:
+        try:
+            r, w, _ = select.select([rcv_sock.ssl_socket, s], [], [])
+            if rcv_sock.ssl_socket in r:
+                buf = rcv_data(rcv_sock, rcv_cn, extend_node)
+                print("rcv_sock:")
+                print(buf)
+                if buf == 404:
+                    continue
+                if len(buf) == 0:
+                    break
+                #print("Sending buf to s")
+                s.send(buf)
+                #print("Echoing to peer")
+                #snd_data(buf, circuit_id, extend_node, rcv_cn, rcv_sock, stream_id)
+            if s in r:
+                buf = s.recv(498)
+                if len(buf) == 0:
+                    break
 
-            buf = s.recv(498)
-            if len(buf) == 0:
-                break
+                snd_data(buf, circuit_id, extend_node, rcv_cn, rcv_sock, stream_id)
 
-            snd_data(buf, circuit_id, extend_node, rcv_cn, rcv_sock)
-
+        except Exception as e:
+            print("Error in receive on socket: " + str(e))
+            continue
 
 def create_sock(target_host, target_port):
 
