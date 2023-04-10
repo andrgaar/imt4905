3df455e9b814f5fa6868126078d2c177c7a74aef
vboxuser <vboxuser@peer1.myguest.virtualbox.org>
2023-04-10 12:07:47 +0200

    p1-4

---
+++ experiments/tests/1004_p1-8_min2_samples30/peer1/startserver.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -x
+
+BASEDIR=$(dirname $0)
+
+SCRIPTPATH="${BASEDIR}/../../rtun-routing"
+
+export PYTHONPATH="${BASEDIR}/../../torpy-rtun-fork/"
+
+# Setup OpenVPN
+#mkdir -p /dev/net
+#mknod /dev/net/tun c 10 200
+#chmod 600 /dev/net/tun
+
+# Download consensus doc
+#rm /root/.local/share/torpy/network_status
+#python3 -m torpy --url https://facebookcorewwwi.onion
+
+# Start rtun server
+#python3 -m cProfile -o cprofile.out "$SCRIPTPATH/rtun.py" -v INFO -g staysafeandgoodluck -f rendezvous.txt -l -t peer1peer2 -n default -i 1 -d 2
+python3 "$SCRIPTPATH/rtun.py" -v INFO -g staysafeandgoodluck -f rendezvous.txt -l -t peer1peer2 -n default -i 1 -d 2
