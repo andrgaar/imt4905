574689b8c9ab408d2207db5ae1ff12e5b497d5f8
vboxuser <vboxuser@peer1.myguest.virtualbox.org>
2023-05-30 13:45:38 +0200

    Opprydding

--- experiments/graphs.py
+++ experiments/graphs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 import networkx as nx
 import numpy as np
 import pandas as p
@@ -11,96 +12,147 @@ from datetime import datetime
 prev_timestamp = None
 epoch_time = datetime(1970, 1, 1)
 
+graphs = []
 topology = {}
+last_time = {} # last time peer seen
+ax = None
+
 
 fname = sys.argv[1]
-ts = None
-if len(sys.argv) == 3:
-    ts = sys.argv[2]
+#offset = int(sys.argv[2])
 
-with open(fname, "r") as f:
+def main():
+    prev_timestamp = None
 
-    edges = {}
-    dead = {}
-    updates = {}
-    dt_diff = 0
-    g_prev = None    
-    diff = None
-    G = None
-    clustering = {}
-    density = {}
+    global ax
+    fig, ax = plt.subplots(figsize=(6,4))
 
-    for line in f:
-        timestamp, peer, event, data = line.strip().split(';')
-
-        if prev_timestamp == None:
-            prev_timestamp = timestamp
-
-        dt_event = datetime.fromtimestamp(float(timestamp))
-        dt_prev = datetime.fromtimestamp(float(prev_timestamp))
-        dt_diff = dt_event - dt_prev
-        
-        x = round(dt_diff.total_seconds())
-        
-        if event == 'TOPOLOGY UPDATE':
-
-            if not peer in edges:
-                edges[peer] = {}
+    with open(fname, "r") as f:
+        for line in f:
+            timestamp, peer, data = line.strip().split(';')
             
-            topology = json.loads(data)
+            if not prev_timestamp:
+                prev_timestamp = timestamp
+
+            dt_event = datetime.fromtimestamp(float(timestamp))
+            dt_prev = datetime.fromtimestamp(float(prev_timestamp))
+            dt_offset = round((dt_event - dt_prev).total_seconds())
+
+            last_time[peer] = dt_event
+            # remove peers not seen in last minute
+            remove = set()
+            for k, v in last_time.items():
+                if (dt_event - v).total_seconds() > 60:
+                    remove.add(k)
+            for k in remove:
+                del last_time[k]
+                graphs.pop(k)
+
+            if peer == "P1":
+                graph = json.loads(data)
+                adjacency_list , graph_nodes, rp_nodes = organizeGraph(graph)
+
+                for k, d in adjacency_list.items():
+                    for ik in d:
+                        d[ik] = {'weight': d[ik] * -1}
+                
+                G = nx.Graph(adjacency_list)
+                
+                graphs.append(G)
+
+
+    ani = FuncAnimation(fig, draw_graph, frames=len(graphs), interval=500, repeat=True)
+    plt.show()
+    
             
-            for k, d in topology.items():
-                for ik in d:
-                    d[ik] = {'weight': d[ik]}
-            
-            G = nx.DiGraph(topology)
+def draw_graph(i):
 
-            clustering[x] = nx.average_clustering(G)
-            density[x] = nx.density(G)
+    global graphs
+    G = graphs.pop(0)
 
-            if ts and ts == timestamp:
-                break
-        
-        if event == 'DEAD ROUTES RECEIVED':
-            dead[x] = peer
-            
-            
-# Clustering 
-print("Clustering:")
-for t in clustering:
-    print(t, clustering[t])
+    global ax
+    ax.clear()
 
-# Density 
-print("Density:")
-for t in density:
-    print(t, density[t])
+    # Topology graph
+    elarge = [(u, v) for (u, v, d) in G.edges(data=True) if d["weight"] > -1000]
+    esmall = [(u, v) for (u, v, d) in G.edges(data=True) if d["weight"] == -1000]
 
-# Topology graph
-elarge = [(u, v) for (u, v, d) in G.edges(data=True) if d["weight"] > 1]
-esmall = [(u, v) for (u, v, d) in G.edges(data=True) if d["weight"] <= 0]
+    init_pos = {'P1':(100,100), 'P2':(200,100),'P3':(50,150),'P4':(250,50),'P5':(50,100),'P6':(250,100),'P7':(50,50),'P8':(250,150)}
 
-pos = nx.spring_layout(G, seed=7)  # positions for all nodes - seed for reproducibility
+    pos = nx.spring_layout(G, pos=init_pos, fixed=['P1'], weight='weight')  # positions for all nodes - seed for reproducibility
+    #pos = nx.circular_layout(G)  # positions for all nodes - seed for reproducibility
+    #pos = nx.kamada_kawai_layout(G, pos=init_pos, weight=None)  # positions for all nodes - seed for reproducibility
 
-# nodes
-nx.draw_networkx_nodes(G, pos, node_size=700)
+    # nodes
+    nx.draw_networkx_nodes(G, pos, node_size=700)
 
-# edges
-nx.draw_networkx_edges(G, pos, edgelist=elarge, width=6)
-nx.draw_networkx_edges(
+    # edges
+    nx.draw_networkx_edges(G, pos, edgelist=elarge, width=3)
+    nx.draw_networkx_edges(
     G, pos, edgelist=esmall, width=6, alpha=0.5, edge_color="b", style="dashed"
-)
+    )
 
-# node labels
-nx.draw_networkx_labels(G, pos, font_size=20, font_family="sans-serif")
-# edge weight labels
-edge_labels = nx.get_edge_attributes(G, "weight")
-nx.draw_networkx_edge_labels(G, pos, edge_labels)
+    # node labels
+    nx.draw_networkx_labels(G, pos, font_size=20, font_family="sans-serif")
+    # edge weight labels
+    edge_labels = nx.get_edge_attributes(G, "weight")
+    #nx.draw_networkx_edge_labels(G, pos, edge_labels)
 
-nx.write_latex(G, 'graph.latex', as_document=False)
+    #nx.write_latex(G, 'graph.latex', as_document=False)
 
-ax = plt.gca()
-ax.margins(0.08)
-plt.axis("off")
-plt.tight_layout()
-plt.show()
+    #ax = plt.gca()
+    #ax.margins(0.08)
+    #plt.axis("off")
+    #plt.tight_layout()
+    plt.title(f"frame {i}")
 
+def organizeGraph(graph_arg):
+
+    # Set to contain nodes within graph
+    nodes = set()
+
+    # Determine nodes in entire topology
+    # and update set of nodes
+    for node in graph_arg:
+        if node[0] not in nodes:
+            nodes.add(node[0])
+            #nodes.add(node[0] + node[3])
+        if node[1] not in nodes:
+            nodes.add(node[1])
+            #nodes.add(node[1]+node[3])
+
+    # Sort nodes alphabetically
+    sorted_nodes = sorted(nodes)
+
+    # Create dict to store all edges between
+    # vertices as an adjacency list
+    new_LL = dict()
+    new_RP = dict() # holds RP of peer
+    for node in sorted_nodes:
+        new_LL[node] = dict()
+        new_RP[node] = dict()
+
+    # Using all link-state advertisement received
+    # from all nodes, create the initial adjacency list
+    # based solely on data received from neighbours
+    for node in sorted_nodes:
+        for link in graph_arg:
+            if node == link[0]:
+                new_LL[node].update({link[1] : link[2]})
+                new_RP[node].update({link[1] : link[3]})
+
+    # Update adjacency list so as to reflect all outgoing/incoming
+    # links (Graph should now fully represent the network topology
+    for node in sorted_nodes:
+        for source_node , cost in new_LL[node].items():
+            new_LL[source_node].update({node : cost})
+        for source_node , rp_node in new_RP[node].items():
+            new_RP[source_node].update({node : rp_node})
+
+
+    # Return adjacency list and least_cost_path dict
+    # to use for Dijkstra Computation
+    return (new_LL , sorted_nodes, new_RP)
+
+if __name__ == '__main__':
+    main()
--- experiments/plot_graphs.py
+++ experiments/plot_graphs.py
@@ -23,24 +23,32 @@ FREQ= '60s'
 CUTOFF=2500
 t0 = None
 
-clustering_df = None
-reliability_df = None
+clustering_df = pd.DataFrame()
+reliability_df = pd.DataFrame()
+deadroutes_df = pd.DataFrame()
+latency_df = pd.DataFrame()
 
 hue_order = ['P1', 'P2', 'P3', 'P4', 'P5', 'P6', 'P7', 'P8']
 
 def main(_set):
     global t0
+
+    cmb_df1 = pd.DataFrame()
+
+    sp_rows = len(argv)
+    if _set in ([2,3]):
+        sp_rows = sp_rows * 2
     #receivelog(arg1)
     #f, ((ax1, ax2), (ax3, ax4), (ax5, ax6)) = plt.subplots(3, 2, sharex=True
     #f, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True, 
-    f, axes = plt.subplots(len(argv), 1, sharex=True, figsize=(12, 6) ,# subplot_kw={'ylim': (0,100)}
+    f, axes = plt.subplots(sp_rows, 1, sharex=True, figsize=(12, 6) ,# subplot_kw={'ylim': (0,100)}
     #f, ax = plt.subplots(1, 1, sharex=True, # subplot_kw={'ylim': (0,100)}
             layout='constrained')
     #ax = plt.figure().add_subplot(projection='3d')
     i = 0
     ax = None
     for arg in argv:
-        if len(argv) == 1:
+        if sp_rows == 1:
             ax = axes
         else:
             ax = axes[i]
@@ -70,10 +78,20 @@ def main(_set):
 
         if _set == 3:
             avgdeg = plot_avgdegree(ax, arg + '/combined-topology.log.csv', None)
-            plot_degree(ax, arg + '/combined-neighbours.csv', title)
+            plot_degree(ax, arg + '/combined-neighbours.csv', 'Node degree - ' + title)
+            sns.despine(fig=None, ax=ax, top=True, right=True, left=False, bottom=False, offset=None, trim=False)
+            ax.grid(axis='y')
+            ax.set_title('Node degree : ' + title, fontsize = 8)
 
-            f.supxlabel('Time (m)')
-            f.supylabel('Node degree')
+            i += 1
+            ax = axes[i]
+            plot_deadroutes(ax, arg + '/combined-deadroutes.csv', 'Dead routes - ' + title)
+            sns.despine(fig=None, ax=ax, top=True, right=True, left=False, bottom=False, offset=None, trim=False)
+            ax.grid(axis='y')
+            ax.set_title('Dead routes : ' + title, fontsize = 8)
+
+            #f.supxlabel('Time (m)')
+            #f.supylabel('Node degree')
         if _set == 4:
             plot_latency(ax, arg + '/combined-sent.log.csv', arg + '/combined-receive.log.csv', title)
             #plot_avgcost(ax, arg + '/combined-topology.log.csv', title)
@@ -96,29 +114,76 @@ def main(_set):
             plot_clustering_vs_reliability(ax, title)
             f.supxlabel('Time (m)')
             f.supylabel('Clustering coeffiecient')
+        if _set == 8:
+            df1 = plot_reliability(None, arg + '/combined-sent.log.csv', arg + '/combined-receive.log.csv', title)
+            df2 = plot_deadroutes(None, arg + '/combined-deadroutes.csv', title)
+            
+            df1.Offset = df1.Offset.round()
+            df2.Offset = df2.Offset.round()
+            
+            combined_df = pd.merge(
+                df1, df2,
+                how="inner",
+                on='Offset',
+                left_on=None,
+                right_on=None,
+                left_index=False,
+                right_index=False,
+                sort=True,
+                suffixes=("_x", "_y"),
+                copy=True,
+                indicator=False,
+                validate=None,
+            )
+            print("combined_df:")
+            print(combined_df)
 
-        #plot_avgcost(ax1, arg + '/combined-topology.log.csv', '')
+            cmb_df1 = pd.concat(
+                [cmb_df1, combined_df],
+                axis=0,
+                join="outer",
+                ignore_index=True,
+                keys=None,
+                levels=None,
+                names=None,
+                verify_integrity=False,
+                copy=True,
+                )
+
+            print("Combined:")
+            print(cmb_df1)
+        if _set == 9:
+            #plot_latency(ax, arg + '/combined-sent.log.csv', arg + '/combined-receive.log.csv', title)
+            plot_avgpathlen(ax, arg + '/combined-topology.log.csv', '')
+        if _set == 10:
+            plot_messages(ax, arg + '/combined-receive.log.csv')
+        if _set == 11:
+            plot_avgcost(ax, arg + '/combined-topology.log.csv', '')
+            
+
         #plot_msgdist(axes[i], arg)
         #plot_routes(ax, arg, "")
         #plot_cost(ax, arg, "")
-        #plot_avgpathlen(ax2, arg + '/combined-topology.log.csv', '')
-        #plot_lookups(ax3, arg + '/combined-receive.log.csv')
         #plot_errors(ax, arg + '/combined-handle_error.log.csv')
         
         sns.despine(fig=None, ax=ax, top=True, right=True, left=False, bottom=False, offset=None, trim=False)
         ax.grid(axis='y')
-        ax.set_title(title, fontsize = 8)
+        #ax.set_title(title, fontsize = 8)
         i += 1
 
+    if _set == 8:
+        plot_deadroutes_vs_reliability(ax, cmb_df1, title)
 
+        
     #f.suptitle('8 peers, min=2', fontsize=12)
-    handles, labels = ax.get_legend_handles_labels()
-    f.legend(handles, labels, loc="upper right")#, bbox_to_anchor=(0.65,1.05))
-    try:
-        for ax in axes:
-            ax.get_legend().remove()
-    except Exception:
-        pass
+    if _set not in([1,3,4,6,9]):
+        handles, labels = ax.get_legend_handles_labels()
+        f.legend(handles, labels, loc="upper right")#, bbox_to_anchor=(0.65,1.05))
+        try:
+            for ax in axes:
+                ax.get_legend().remove()
+        except Exception:
+            pass
 
     plt.show()
 
@@ -134,18 +199,14 @@ def to_offset(df, set_index=False):
 
     return df
 
-def plot_clustering_vs_reliability(ax, title):
+def plot_deadroutes_vs_reliability(ax, df, title):
 
-    clustering_df.Offset = clustering_df.Offset.round()
-    reliability_df.Offset = reliability_df.Offset.round()
+    df = df.rename(columns = {'Peer':'Dead routes', 'SuccessRate':'Success rate'})
 
-    combined_df = pd.concat([clustering_df, reliability_df]
-                            , axis = 1
-                            , ignore_index=True
-                            )
-                        
+    print("Dead routes vs reliability")
+    print(df)
 
-    print(combined_df)
+    g = sns.relplot(kind="line", data=df, x='Dead routes', y='Success rate');
 
 def plot_reliability(ax, sent, receive, title):
     FREQ = '60s'
@@ -154,7 +215,7 @@ def plot_reliability(ax, sent, receive, title):
     sent_df['Timestamp'] = pd.to_datetime(sent_df['Timestamp']).dt.tz_localize('UTC')
     sent_df = to_offset(sent_df).reset_index()
     sent_df = sent_df[['Offset', 'ID']]
-    print(sent_df)
+    #print(sent_df)
 
     receive_df = pd.read_csv(receive, sep=";")
     receive_df = receive_df.loc[receive_df['Received'] == 'LOOKUP']
@@ -163,7 +224,7 @@ def plot_reliability(ax, sent, receive, title):
     receive_df = to_offset(receive_df).reset_index()
     receive_df['Result'] = 'Success'
     receive_df = receive_df[['ID', 'Result']]
-    print(receive_df)
+    #print(receive_df)
 
     result = pd.merge(
     sent_df,
@@ -181,6 +242,11 @@ def plot_reliability(ax, sent, receive, title):
     validate=None,
     )
     result = result.replace(np.nan, 'Failed')
+
+    failed_df = result
+    failed_df = failed_df.loc[failed_df['Result'] == 'Failed']
+    print(failed_df.to_string())
+
     result.set_index('Offset', inplace=True)
     result = result.groupby([pd.Grouper(freq = FREQ), 'Result'])['Result'].size().unstack().reset_index()
     result['Offset'] = result['Offset'].dt.total_seconds() / 60
@@ -191,9 +257,9 @@ def plot_reliability(ax, sent, receive, title):
     except Exception:
         result['SuccessRate'] = 100
 
-    global reliability_df
-    reliability_df = result
-    print(result)
+    print("Reliability")
+    #print(result)
+    print(result.describe())
 
     p = sns.lineplot(ax=ax, data=result, x='Offset', y='SuccessRate', legend=True)
     #p = sns.barplot(ax=ax, data=result, x='Offset', y='SuccessRate')
@@ -204,6 +270,8 @@ def plot_reliability(ax, sent, receive, title):
     #print(xticks)
     #p.set_xticks(xticks, xticks) # <--- set the ticks first
 
+    return result
+
 def plot_latency(ax, sent, receive, title):
     FREQ = '60s'
 
@@ -211,8 +279,8 @@ def plot_latency(ax, sent, receive, title):
     sent_df['Timestamp'] = pd.to_datetime(sent_df['Timestamp']).dt.tz_localize('UTC')
     sent_df = to_offset(sent_df).reset_index()
     sent_df = sent_df[['Timestamp', 'Offset', 'ID', 'Route', 'Shortest']]
-    print(sent)
-    print(sent_df)
+    #print(sent)
+    #print(sent_df)
 
     receive_df = pd.read_csv(receive, sep=";")
     receive_df = receive_df.loc[receive_df['Received'] == 'LOOKUP']
@@ -221,8 +289,7 @@ def plot_latency(ax, sent, receive, title):
     receive_df = to_offset(receive_df).reset_index()
     #receive_df['Result'] = 'Success'
     receive_df = receive_df[['Timestamp', 'ID']]
-    print(receive)
-    print(receive_df)
+    #print(receive_df)
 
     result = pd.merge(
     sent_df,
@@ -239,31 +306,44 @@ def plot_latency(ax, sent, receive, title):
     indicator=False,
     validate=None,
     )
+
     print("sent JOIN receive")
-    print(result)
-    #result = result.replace(np.nan, 0)
+    print(result.to_string())
+
     result['Latency'] = result.Timestamp_y - result.Timestamp_x
     result['Latency'] = result['Latency'].dt.total_seconds() * 1000
-    #result = result.loc[result['Latency'] <= 1000]
-    result.set_index('Offset', inplace=True)
-    result = result.groupby([pd.Grouper(freq = FREQ),'Shortest'])['Latency'].mean().reset_index()
-    result['Offset'] = result['Offset'].dt.total_seconds() / 60
-    #result = result.reset_index()
+    result = result.loc[result['Latency'] < 1000]
+    #result.set_index('Offset', inplace=True)
+    #result = result.groupby([pd.Grouper(freq = FREQ),'Shortest','Route'])['Latency'].mean().reset_index()
+    result['Time_min'] = result['Offset'].dt.total_seconds() / 60
+    #result['Offset'] = result['Offset'].dt.total_seconds()
+    #result['Offset'] = result['Offset'].round(0)
     result = result.replace(True, "Shortest (Dijkstra)")
     result = result.replace(False, "Others")
     result = result.rename(columns={"Shortest": "Actual latency"})
-    result = result.loc[result['Offset'] <= CUTOFF]
+    #result = result.loc[result['Offset'] <= CUTOFF]
     max_y = result.Latency.max() + 100
     print("Latencies")
-    print(result)
+    print(result.to_string())
+    #print(result.describe())
+
+    global latency_df
+    latency_df = result
 
     #p = sns.lineplot(ax=ax, data=result, x='Offset', y='Latency', hue='Actual latency', style='Actual latency', legend=True)
-    p = sns.lineplot(ax=ax, data=result, x='Offset', y='Latency', legend=True)
-    ax.set_title(title)
+    #p = sns.lineplot(ax=ax, data=result, x='Offset', y='Latency', hue='Route', legend=True)
+    p = sns.scatterplot(ax=ax, data=result, x='Time_min', y='Latency', hue='Route', legend=True)
+
+    shortest_df = result.loc[result['Actual latency'] == 'Shortest (Dijkstra)']
+    p = sns.lineplot(ax=ax, data=shortest_df, x='Time_min', y='Latency', color='black', size='Actual latency', legend=True)
+
+    xlabels = [int(x) for x in p.get_xticks()]
+    p.set_xticklabels(xlabels)
+
+    #ax.set_title(title)
     #ax.set(xlabel='Time (s)', ylabel='Latency (ms)')
     ax.set(xlabel=None, ylabel=None)
-    p.set_ylim(0, 1200)
-    #p.set_xticklabels(['2011','2012','2013','2014','20
+    #p.set_ylim(0, 1200)
 
     # Volume
     #sent_df = sent_df[['Timestamp', 'Offset', 'ID', 'Route', 'Shortest']]
@@ -281,33 +361,112 @@ def plot_latency(ax, sent, receive, title):
     #p2.set_xticks(xticks, xticks) # <--- set the ticks first
     #print(xticks)
 
+    print("-----------------------")
+    print("Best each minute")
+    ax2 = plt.figure().add_subplot()
+
+    shortest_df.set_index('Offset', inplace=True)
+    shortest_df = shortest_df.groupby([pd.Grouper(freq = '60s')])['Latency'].mean().reset_index()
+    shortest_df['Time_min'] = shortest_df['Offset'].dt.total_seconds() / 60
+    shortest_df['Mean latency'] = 'Selected path'
+
+    sns.lineplot(ax=ax2, data=shortest_df, x='Time_min', y='Latency', hue='Mean latency', palette=['black'], legend=True)
+    
+
+    result.set_index('Offset', inplace=True)
+    grp_df = result.groupby([pd.Grouper(freq = '60s')])['Latency'].mean().reset_index()
+    grp_df['Time_min'] = grp_df['Offset'].dt.total_seconds() / 60
+    grp_df['Mean latency'] = 'All paths'
+    #grp_df = grp_df[['Time_min', 'Actual latency', 'Latency']]
+    print(grp_df.to_string())
+
+    p2 = sns.lineplot(ax=ax2, data=grp_df, x='Time_min', y='Latency', hue='Mean latency', palette=['orange'], legend=True)
+
+    xlabels = [int(x) for x in p2.get_xticks()]
+    p2.set_xticklabels(xlabels)
+    ax2.set(xlabel='Time (m)', ylabel='Latency (ms)')
+
 def plot_deadroutes(ax, csvfile, title):
     df = pd.read_csv(csvfile, sep=";")
     df['Timestamp'] = pd.to_datetime(df['Timestamp']).dt.tz_localize('UTC')
     df = to_offset(df).reset_index()
     df = df[['Offset', 'Peer', 'DeadPeer']]
+
+    df.set_index('Offset', inplace=True)
+    df = df.groupby([pd.Grouper(freq = FREQ)])['Peer'].size().reset_index()
+    
     df['Offset'] = df['Offset'].dt.total_seconds() / 60
-    df = df.loc[df['Offset'] <= CUTOFF]
-    #df.set_index('Offset', inplace=True)
+    #df = df.loc[df['Offset'] <= CUTOFF]
+    print("Deadroutes")
     print(df)
-    p = sns.scatterplot(ax=ax, data=df, x='Offset', y='Peer', hue='DeadPeer', legend=True)
-    #ax.set(xlabel='Time (s)', ylabel='Peer')
-    ax.set(xlabel=None, ylabel=None)
+    #p = sns.scatterplot(ax=ax, data=df, x='Offset', y='Peer', hue='DeadPeer', legend=True)
+    #ax.set(xlabel='Time (m)', ylabel='Peer detecting')
+
+    p = sns.lineplot(ax=ax, data=df, x='Offset', y='Peer', legend=True)
+
+    #ax.set(xlabel=None, ylabel=None)
     #ax.grid(axis='y')
+
+    return df
 
 def plot_avgpathlen(ax, csvfile, title):
     df = pd.read_csv(csvfile, sep=";")
+    df['Timestamp'] = pd.to_datetime(df['Timestamp']).dt.tz_localize('Europe/Amsterdam').dt.tz_convert('UTC')
     df = to_offset(df).reset_index()
-    df = df[['Offset', 'AvgPathLen']]
+    df = df[['Offset', 'AvgPathLen', 'AvgCost']]
+    df = df.loc[df['AvgPathLen'] > 0]
+    df = df.loc[df['AvgCost'] < 1000]
     df.set_index('Offset', inplace=True)
-
-    df = df.groupby([pd.Grouper(freq = '30s')])['AvgPathLen'].mean().reset_index()
-    df['Offset'] = df['Offset'].dt.total_seconds()
-    df = df.loc[df['Offset'] <= CUTOFF]
     print(df)
 
-    df.plot(ax=ax, x='Offset', y='AvgPathLen', kind='line', colormap="tab20", legend=False,
-          xlabel = "Time (s)", ylabel = "Avg. path length")
+    #df = df.groupby([pd.Grouper(freq = '60s')])['AvgPathLen'].mean().reset_index()
+    df = df.groupby([pd.Grouper(freq = '60s')]).agg(MeanPathLen=('AvgPathLen', np.mean), MeanCost=('AvgCost', np.mean)).reset_index()
+    df['Offset'] = df['Offset'].dt.total_seconds() / 60
+
+    print(df)
+    
+    
+    #dfm = df.melt('Offset', var_name='Parameter', value_name='Value')
+    #df1 = df[['Offset', 'MeanPathLen']]
+    #df2 = df[['Offset', 'MeanCost']]
+    
+    #sns.relplot(ax=ax, data=df, x='AvgPathLen', y='AvgCost')
+
+    p2 = sns.lineplot(ax=ax, data=df, x='Offset', y='MeanCost', color="orange", linestyle="dashed", label='Cost', legend=False)
+    ax.set(xlabel='Time (m)', ylabel='Mean path cost')
+    ax.legend(loc='upper right')
+    ax2 = ax.twinx()
+    p1 = sns.lineplot(ax=ax2, data=df, x='Offset', y='MeanPathLen', color="blue", label='Path length', legend=False)
+    ax2.set(xlabel='Time (m)', ylabel='Mean path length')
+    ax2.legend(loc='upper center')
+
+
+    global latency_df
+    if not latency_df.empty:
+        
+        df.Offset = df.Offset.round(0)
+        latency_df.Offset = latency_df.Offset.round(0)
+        print("Latency:")
+        print(latency_df)
+
+        result = pd.merge(
+            df,
+            latency_df,
+            how="left",
+            on='Offset',
+            left_on=None,
+            right_on=None,
+            left_index=False,
+            right_index=False,
+            sort=True,
+            suffixes=("_x", "_y"),
+            copy=True,
+            indicator=False,
+            validate=None,
+        )
+        print(result)
+        p3 = sns.lineplot(ax=ax, data=result, x='Offset', y='Latency', color="red", label='Actual latency', legend=True)
+
 
 def plot_avgdegree(ax, csvfile, title):
     df = pd.read_csv(csvfile, sep=";")
@@ -340,8 +499,8 @@ def plot_degree(ax, csvfile, title):
     print(df)
 
     p = sns.lineplot(ax=ax, data=df, x='Offset', y='NodeDegree', hue='Peer', hue_order=hue_order, legend=True)
-    #p.set(xlabel='Time (s)', ylabel='Node degree')
-    p.set(xlabel=None, ylabel=None)
+    p.set(xlabel='Time (m)', ylabel='Node degree')
+    #p.set(xlabel=None, ylabel=None)
     p.set_ylim(0, 8)
 
     
@@ -388,12 +547,11 @@ def plot_avgcost(ax, csvfile, title):
     df = pd.read_csv(csvfile, sep=";")
     df['Timestamp'] = pd.to_datetime(df['Timestamp']).dt.tz_localize('Europe/Amsterdam').dt.tz_convert('UTC')
     df = to_offset(df).reset_index()
-    df = df[['Offset', 'AvgCost']]
+    df = df[['Offset', 'AvgCost', 'AvgPathLen']]
     df.set_index('Offset', inplace=True)
 
-    df = df.groupby([pd.Grouper(freq = FREQ)])['AvgCost'].mean().reset_index()
+    df = df.groupby([pd.Grouper(freq = FREQ)]).agg(MeanPathLen=('AvgPathLen', np.mean), MeanCost=('AvgCost', np.mean)).reset_index()
     df['Offset'] = df['Offset'].dt.total_seconds() / 60
-    #df = df.loc[df['Offset'] <= CUTOFF]
     print(df)
 
     p = sns.lineplot(ax=ax, data=df, x='Offset', y='AvgCost', legend=True)
@@ -463,23 +621,23 @@ def plot_lsa(ax, csvfile):
     csv.plot()
 
 
-def plot_lookups(ax, csvfile):
+def plot_messages(ax, csvfile):
     csv = pd.read_csv(csvfile, sep=";")
 
-    csv['Timestamp'] = pd.to_datetime(csv['Timestamp'])
     csv['Timestamp'] = pd.to_datetime(csv['Timestamp']).dt.tz_localize('Europe/Amsterdam').dt.tz_convert('UTC')
-    csv = csv.loc[csv['Received'] == "LOOKUP"]
+    csv = csv.loc[csv['Received'] == "LSA"]
+    csv = csv.loc[csv['Peer'] == "P1"]
     csv = to_offset(csv)
     csv = csv[['Offset', 'Peer']]
     csv.set_index('Offset', inplace=True)
 
-    csv = csv.groupby([pd.Grouper(freq = '1min'), 'Peer']).size().reset_index(name='Received')
-    csv['Offset'] = csv['Offset'].dt.total_seconds()
+    csv = csv.groupby([pd.Grouper(freq = '1min'),'Peer']).size().reset_index(name='Received')
+    csv['Offset'] = csv['Offset'].dt.total_seconds() / 60
 
     print(csv)
 
-    p = sns.lineplot(ax=ax, data=csv, x='Offset', y='Received', hue='Peer', legend=False)
-    p.set(xlabel='Time (s)', ylabel='Msgs rcvd. / min')
+    p = sns.lineplot(ax=ax, data=csv, x='Offset', y='Received', hue='Peer', legend=True)
+    p.set(xlabel='Time (m)', ylabel='Msgs rcvd. / min')
 
 
 def plot_cost(ax, csvfile, title):
@@ -536,13 +694,14 @@ def plot_convergence(ax, csvfile, title):
     #csv = csv.groupby(['Offset'])['Convergence'].mean().reset_index()
     #csv.where(filter, inplace = True)
     csv['Offset'] = csv['Offset'].dt.total_seconds() / 60
-    csv = csv.loc[csv['Offset'] <= CUTOFF]
+    #csv = csv.loc[csv['Offset'] <= CUTOFF]
     print(csv)
     
     #p = sns.pointplot(ax=ax, data=csv, x='Offset', y='Convergence')
     p = sns.lineplot(ax=ax, data=csv, x='Offset', y='Convergence', legend=True)
 
     p.set(xlabel = None, ylabel = None)
+    #p.set_xlim(0, 30)
     p.set_ylim(0, 105)
     ax.set_title(title, fontsize = 8)
     #xticks = range(len(csv))
